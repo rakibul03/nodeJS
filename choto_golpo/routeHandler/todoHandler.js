88bd72ea0469aac181c04cb10ac76096dc63713e
Md Rakibul Hasan <rakibhasan0308@gmail.com>
2023-02-01 21:58:37 +0530

    module exports to app

--- choto_golpo/routeHandler/todoHandler.js
+++ choto_golpo/routeHandler/todoHandler.js
@@ -2,10 +2,14 @@ const express = resquire("express");
 const router = express.Router();
 
 // GET ALL THE TODOS
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+  res.send("Todo route is running");
+});
 
 // GET A TODOS by ID
 router.get("/:id", async (req, res) => {});
 
 // POST TODO
 router.post("/", async (req, res) => {});
+
+module.exports = router;
